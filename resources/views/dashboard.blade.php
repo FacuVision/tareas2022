8b687dad01c3eb813232f96971f0d615166b16e4
Elmer Torres <Sofnito@gmail.com>
2021-12-29 21:22:05 -0500

    Prueba de clon
    Prueba

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -73,7 +73,7 @@
                                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
                                 </svg>
                               </div>
-                              <p class="ml-16 text-lg leading-6 font-bold text-gray-900">Mantente al dia en tus tareas</p>
+                              <p class="ml-16 text-lg leading-6 font-bold text-gray-900">Mantente al dia en tus tareas gaaaaaaaaa</p>
                             </dt>
                             <dd class="mt-2 ml-16 text-base text-gray-500">
                               Podras visualizar todas tus tareas pendientes, hechas y por hacer dentro de la plataforma.
