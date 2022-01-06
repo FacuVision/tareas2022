7e16ed193a09c7dbd1fd3477e9e66f0c25565c42
Elmer Torres <Sofnito@gmail.com>
2022-01-06 17:14:11 -0500

    Modal Respuestas
    -EL modal de respuestas tiene un scroll por si la pregunta es demasiado larga

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
         <link rel="stylesheet" href="https://unpkg.com/tailwindcss/dist/tailwind.min.css" />
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-
+        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
 
         @livewireStyles
 
--- resources/views/livewire/show-respuesta.blade.php
+++ resources/views/livewire/show-respuesta.blade.php
@@ -9,34 +9,106 @@
 
     <x-jet-dialog-modal maxWidth="2xl" wire:model="show">
         <x-slot name="title">
-            Tarea {{ $dato->titulo }}
+            <center>{{ $dato->titulo }}</center>
         </x-slot>
 
         <x-slot name="content">
-            {{--  --}}
-            <div class="text-left">
-                {{--  --}}
-                @php
-                    //$i = 0;
-                    $count = 1;
-                @endphp
 
-                {{-- <textarea  cols="70" rows="10">@foreach ($res as $re)
+
+            <div class="text-left">
+            @php
+                //$i = 0;
+                $count = 1;
+            @endphp
+
+            {{-- <textarea  cols="70" rows="10">@foreach ($res as $re)
                     {{$re->actividad_id}}
                 @endforeach</textarea> --}}
-                @foreach ($dato->actividades as $act)
+            @foreach ($dato->actividades as $act)
+
                     <x-jet-label class="text-left pt-2 pb-2 text-indigo-600" value="Actividad {{ $count }} :" />
 
-                    @switch($act->tipo)
-                        @case(0)
-                            {{-- PREGUNTA CORTA --}}
-                            <div class="overflow-auto break-words text-left pb-2 md:break-all  md:text-left">
-                                <p>
-                                    {{ $act->descripcion }}
-                                </p>
-                            </div>
+                @switch($act->tipo)
+                    @case(0)
+                        {{-- PREGUNTA CORTA --}}
+                        <div style="overflow: auto; padding: 5px">
+                                {{ $act->descripcion }}
+                        </div>
 
-                            <p class="text-left text-md text-navy-600  font-bold">Respuesta: </p>
+
+                        <p class="text-left text-md text-navy-600  font-bold">Respuesta: </p>
+                        @foreach ($res as $re)
+                            @if ($re->actividad_id == $act->id)
+                                <input disabled
+                                    class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm w-full"
+                                    type="text" value="{{ $re->descripcion }}">
+                            @endif
+                        @endforeach
+                        {{-- <input required wire:model.defer="descripcion.{{ $act->id }}" name="descripcion[]"
+                                class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm w-full"
+                                type="text"> --}}
+                    @break
+
+                    @case(1)
+                        {{-- PREGUNTA LARGA --}}
+                        <div class="overflow-auto justify-start pb-2 break-words md:break-all text-left md:text-left">
+                            <p>
+                                {{ $act->descripcion }}
+                            </p>
+                        </div>
+                        <p class="text-md text-navy-600 font-bold">Respuesta: </p>
+                        @foreach ($res as $re)
+                            @if ($re->actividad_id == $act->id)
+                                <textarea id="editor" disabled class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm w-full w-full">{{ $re->descripcion }}</textarea>
+                            @endif
+                        @endforeach
+                        {{-- <textarea required id="editor" wire:model.defer="descripcion.{{ $act->id }}"
+                                name="descripcion[]" class="w-full">
+
+                            </textarea> --}}
+
+                    @break
+
+                    @case(2)
+                        {{-- LINK DE VIDEO --}}
+                        @php
+                            $link = preg_replace('/\s*[a-zA-Z\/\/:\.]*youtu(be.com\/watch\?v=|.be\/)([a-zA-Z0-9\-_]+)([a-zA-Z0-9\/\*\-\_\?\&\;\%\=\.]*)/i', " https://www.youtube.com/embed/$2", $act->recurso);
+                        @endphp
+                        <div class="justify-start pb-2 break-words md:break-all text-left md:text-left">
+                            <p>
+                                {{ $act->descripcion }}
+                            </p>
+                        </div>
+                        <div class="relative" style="padding-top: 56.25%">
+                            <iframe class="absolute inset-0 w-full h-full pb-2" src="{{ $link }}" frameborder="0"
+                                …></iframe>
+                        </div>
+                        <p class="text-md text-navy-600 font-bold">Respuesta: {{ $act->id }}</p>
+                        @foreach ($res as $re)
+                            @if ($re->actividad_id == $act->id)
+                                <textarea id="editor" disabled class="w-full">{{ $re->descripcion }}</textarea>
+                            @endif
+                        @endforeach
+                        {{-- <textarea required id="editor" wire:model.defer="descripcion.{{ $act->id }}"
+                                name="descripcion[]" class="w-full">
+                            </textarea> --}}
+                    @break
+
+                    @case(3)
+                        {{-- LINK DE CARPETA DE DRIVE --}}
+                        <div class="justify-start pb-2 break-words md:break-all text-left md:text-left">
+                            <p>
+                                {{ $act->descripcion }}
+                            </p>
+                        </div>
+                        <div class="text-left">
+
+                            <a href="{{ $act->recurso }}" target="_blank" class="text-xl text-indigo-600 font-bold">
+                                <div class="pb-6 block">
+                                    <i class="text-center fab fa-google-drive fa-2x"></i>
+                                    Abrir en drive
+                                </div>
+                            </a>
                             @foreach ($res as $re)
                                 @if ($re->actividad_id == $act->id)
                                     <input disabled
@@ -44,90 +116,17 @@
                                         type="text" value="{{ $re->descripcion }}">
                                 @endif
                             @endforeach
-                            {{-- <input required wire:model.defer="descripcion.{{ $act->id }}" name="descripcion[]"
-                                class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm w-full"
-                                type="text"> --}}
-                        @break
+                        </div>
+                    @break
 
-                        @case(1)
-                            {{-- PREGUNTA LARGA --}}
-                            <div class="overflow-auto justify-start pb-2 break-words md:break-all text-left md:text-left">
-                                <p>
-                                    {{ $act->descripcion }}
-                                </p>
-                            </div>
-                            <p class="text-md text-navy-600 font-bold">Respuesta: </p>
-                            @foreach ($res as $re)
-                                @if ($re->actividad_id == $act->id)
-                                    <textarea id="editor" disabled class="w-full">{{ $re->descripcion }}</textarea>
-                                @endif
-                            @endforeach
-                            {{-- <textarea required id="editor" wire:model.defer="descripcion.{{ $act->id }}"
-                                name="descripcion[]" class="w-full">
-
-                            </textarea> --}}
-
-                        @break
-
-                        @case(2)
-                            {{-- LINK DE VIDEO --}}
-                            @php
-                                $link = preg_replace('/\s*[a-zA-Z\/\/:\.]*youtu(be.com\/watch\?v=|.be\/)([a-zA-Z0-9\-_]+)([a-zA-Z0-9\/\*\-\_\?\&\;\%\=\.]*)/i', " https://www.youtube.com/embed/$2", $act->recurso);
-                            @endphp
-                            <div class="justify-start pb-2 break-words md:break-all text-left md:text-left">
-                                <p>
-                                    {{ $act->descripcion }}
-                                </p>
-                            </div>
-                            <div class="relative" style="padding-top: 56.25%">
-                                <iframe class="absolute inset-0 w-full h-full pb-2" src="{{ $link }}"
-                                    frameborder="0" …></iframe>
-                            </div>
-                            <p class="text-md text-navy-600 font-bold">Respuesta: {{ $act->id }}</p>
-                            @foreach ($res as $re)
-                                @if ($re->actividad_id == $act->id)
-                                    <textarea id="editor" disabled
-                                        class="w-full">{{ $re->descripcion }}</textarea>
-                                @endif
-                            @endforeach
-                            {{-- <textarea required id="editor" wire:model.defer="descripcion.{{ $act->id }}"
-                                name="descripcion[]" class="w-full">
-                            </textarea> --}}
-                        @break
-
-                        @case(3)
-                            {{-- LINK DE CARPETA DE DRIVE --}}
-                            <div class="justify-start pb-2 break-words md:break-all text-left md:text-left">
-                                <p>
-                                    {{ $act->descripcion }}
-                                </p>
-                            </div>
-                            <div class="text-left">
-
-                                <a href="{{ $act->recurso }}" target="_blank" class="text-xl text-indigo-600 font-bold">
-                                    <div class="pb-6 block">
-                                        <i class="text-center fab fa-google-drive fa-2x"></i>
-                                        Abrir en drive
-                                    </div>
-                                </a>
-                                @foreach ($res as $re)
-                                    @if ($re->actividad_id == $act->id)
-                                        <input disabled
-                                            class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm w-full"
-                                            type="text" value="{{ $re->descripcion }}">
-                                    @endif
-                                @endforeach
-                            </div>
-                        @break
-
-                    @endswitch
+                @endswitch
 
 
-                    @php
-                        //$i++;
-                        $count++;
-                    @endphp
-                @endforeach
+                @php
+                    //$i++;
+                    $count++;
+                @endphp
+            @endforeach
 
             </div>
 
